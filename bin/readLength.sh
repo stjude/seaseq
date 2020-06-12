76810a3605cc6b787419df1942fd1e55f329f09e
Adetunji <madetunj@splprhpc06.cm.cluster>
2020-06-12 13:39:46 -0500

    not needed readlength.sh

--- bin/readLength.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-#getting the readLength of the fastq reads
-fastqfile=$1;
-
-readlength=$(zcat $fastqfile | head -n 2 | tail -n 1 | awk '{print length}');
-echo $readlength  > $readlength.rdl;
