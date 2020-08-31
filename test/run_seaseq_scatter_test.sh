708e3848ed04b793d2cfa8bbf3195167ebca34b8
Adetunji <madetunj@splprhpc05.cm.cluster>
2020-08-31 14:50:53 -0500

    seaseq scripts

--- test/run_seaseq_scatter_test.sh
+++
@@ -1,1 +0,0 @@
-bsub -P watcher -q compbio -J seaseqscat -o seaseqscat-out -e seaseqscat-err -N ../seaseq_wrap_scatter.sh inputymlscatter.yml SEASEQSCATTER
--- test/run_seaseq_test.sh
+++
@@ -1,2 +0,0 @@
-bsub -P watcher -q compbio -J seaseq -o seaseq-out -e seaseq-err -N ../seaseq_wrap.sh inputyml.yml SEASEQ
-
